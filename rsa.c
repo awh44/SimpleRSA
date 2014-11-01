6c3fb0fa204fbc93951f00bed7935b148e652323
Austin Herring <awh44@drexel.edu>
2014-11-01 09:11:42 -0400

    Took out the 'backwardness' from the sieve'

--- rsa.c
+++ rsa.c
@@ -5,21 +5,15 @@
 
 #include "../BitVector/BitVector.h"
 
-//treat the BitVector as "backwards" - i.e., mark the composites as true, not the primes
 void apply_sieve(BitVector *bv, int max)
 {
 	int i;
 	for (i = 2; i * i <= max; i++)
 	{
-		if (GET_BIT(bv, i - 2))
-		{
-			continue;
-		}
-
 		int j;
 		for (j = i + i; j < max; j += i)
 		{
-			SET_BIT(bv, j - 2);
+			UNSET_BIT(bv, j - 2);
 		}
 	}
 }
@@ -30,18 +24,25 @@ int main()
 	int n;
 	scanf("%d", &n);
 
+	if (n < 0)
+	{
+		printf("Sorry, that number is too big or is negative. Try again.\n");
+		return 1;
+	}
+
 	BitVector primes;
 	INITIALIZE(&primes, n);
+	SET_ALL_BITS(&primes);
 
+	printf("Applying sieve...\n");
 	apply_sieve(&primes, n + 2);
 
 	int count = 0;
 	int i;
 	for (i = 0; i < n; i++)
 	{
-		if (!GET_BIT(&primes, i))
+		if (GET_BIT(&primes, i))
 		{
-			//printf("%d\n", i + 2);
 			count++;
 		}
 	}
